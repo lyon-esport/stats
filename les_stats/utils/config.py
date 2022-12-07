f66b3ec399e00fc653c2dcf8a98d95c3b9c895ca
Ludovic Ortega <ludovic.ortega@lyon-esport.fr>
2022-12-07 21:53:15 +0000

    [Fix] Poetry tag & cors setting

--- les_stats/utils/config.py
+++ les_stats/utils/config.py
@@ -1,16 +1,16 @@
-from typing import List, Optional
+from typing import Any, List, Optional
 
 import pytz
 from pydantic import BaseSettings, HttpUrl, validator
 
 
 class Settings(BaseSettings):
-    APP_HOST: str = "localhost"
+    APP_HOST: str = "127.0.0.1"
     APP_PORT: int = 8000
     TIMEZONE: str
     DB_URL: str
     SALT: str
-    EXPORTER_ADDR: str = "localhost"
+    EXPORTER_ADDR: str = "127.0.0.1"
     EXPORTER_PORT: int = 9345
     VALORANT_API_KEY: str = None
     VALORANT_API_ROUTING: str = None
@@ -32,16 +32,8 @@ class Settings(BaseSettings):
             raise ValueError("Invalid timezone")
         return v
 
-    @validator("BACKEND_CORS_ORIGINS", pre=True)
-    def assemble_cors_origins(cls, v: str) -> List[HttpUrl]:
-        if v.startswith("[") and v.endswith("]"):
-            return [i.strip() for i in v[1:-1].split(",")]
-        elif len(v) == 0:
-            return []
-        return ValueError(v)
-
     @validator("SENTRY_DSN", pre=True)
-    def sentry_dsn_can_be_blank(cls, v: str) -> Optional[str]:
+    def sentry_dsn_can_be_blank(cls, v: str) -> Optional[HttpUrl]:
         if len(v) == 0:
             return None
         return v
@@ -49,6 +41,12 @@ class Settings(BaseSettings):
     class Config:
         env_prefix = "LES_STATS_"
         env_file = ".env"
+
+        @classmethod
+        def parse_env_var(cls, field_name: str, raw_val: str) -> Any:
+            if field_name == "BACKEND_CORS_ORIGINS":
+                return raw_val.split(",")
+            return cls.json_loads(raw_val)
 
 
 def get_settings() -> Settings:
